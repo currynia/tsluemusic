c7cb761a4b6216b887146a5774b24aa678dbb78e
Rui <teewenrui31@gmail.com>
2025-12-29 14:30:50 +0800

    Add command

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,7 +4,7 @@ import { apiRouter } from './routes/api.js'
 
 export const startServer = () => {
   const server = express()
-  const port = 3000
+  const port = 50000
 
   server.use(express.static(path.join('dist', 'client')))
 
